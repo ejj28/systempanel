aafd77c58f2f9eb6ae62a462c9964a01d7cd304a
ejj28 <eriksinventions11@gmail.com>
2020-08-22 23:06:57 -0400

    Mostly UI stuff

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const si = require('systeminformation');
 const app = express();
 const port = 3000;
+const path = require('path');
 
-app.use(express.static('web'));
+app.use('/', express.static(path.join(__dirname, "web")));
 
 app.get("/cpuinfo", (req, res, next) => {
 	si.cpu(function (data) {
@@ -13,8 +14,13 @@ app.get("/cpuinfo", (req, res, next) => {
 
 app.get("/cputemp", (req, res, next) => {
 	si.cpuTemperature(function (data) {
-		console.log(data.cores)
 		res.json({ "temp": data.cores[0]});
+	})
+});
+
+app.get("/osinfo", (req, res, next) => {
+	si.osInfo(function (data) {
+		res.json({ "hostname": data.hostname});
 	})
 });
 
--- web/index.js
+++ web/index.js
@@ -1,5 +1,5 @@
-var app = new Vue({
-	el: '#app',
+var vueCpuInfo = new Vue({
+	el: '#vueCpuInfo',
 	data: {
 		cpuMfg: '',
 		cpuModel: '',
@@ -7,32 +7,56 @@ var app = new Vue({
 	}
 })
 
-const Http = new XMLHttpRequest();
-const url = 'http://localhost:3000/cpuinfo';
-Http.open("GET", url);
-Http.send();
+var sidebar = new Vue({
+	el: '#navcol',
+	data: {
+		hostname: 'SystemPanel'
+	}
+})
 
-Http.onreadystatechange = (e) => {
-	var cpuData = JSON.parse(Http.responseText);
-	app.cpuMfg = cpuData.mfg;
-	app.cpuModel = cpuData.model;
-	if (app.cpuMfg.includes("Intel")) {
-		$('#cpuManufacturer').css('color', 'blue');
-	} else if (app.cpuMfg.includes("AMD")) {
-		$('#cpuManufacturer').css('color', 'red');
+function getOSInfo() {
+	var Http = new XMLHttpRequest();
+	var url = 'http://localhost:3000/osinfo';
+	Http.open("GET", url);
+	Http.send();
+
+	Http.onreadystatechange = (e) => {
+		var osData = JSON.parse(Http.responseText);
+		sidebar.header = osData.hostname;
+		
+	}
+}
+
+function getCpuInfo() {
+	var Http = new XMLHttpRequest();
+	var url = 'http://localhost:3000/cpuinfo';
+	Http.open("GET", url);
+	Http.send();
+
+	Http.onreadystatechange = (e) => {
+		var cpuData = JSON.parse(Http.responseText);
+		vueCpuInfo.cpuMfg = cpuData.mfg;
+		vueCpuInfo.cpuModel = cpuData.model;
+		if (vueCpuInfo.cpuMfg.includes("Intel")) {
+			$('#cpuManufacturer').css('color', 'blue');
+		} else if (vueCpuInfo.cpuMfg.includes("AMD")) {
+			$('#cpuManufacturer').css('color', 'red');
+		}
 	}
 }
 
 function getTemp() {
-	const tempRequest = new XMLHttpRequest();
-	const tempRequestURL = 'http://localhost:3000/cputemp';
+	var tempRequest = new XMLHttpRequest();
+	var tempRequestURL = 'http://localhost:3000/cputemp';
 	tempRequest.open("GET", tempRequestURL);
 	tempRequest.send();
 
 	tempRequest.onreadystatechange = (e) => {
 		var cpuTemp = JSON.parse(tempRequest.responseText);
-		app.cpuTemp = cpuTemp.temp;
+		vueCpuInfo.cpuTemp = cpuTemp.temp;
 	}
 }
 
+getOSInfo();
+getCpuInfo();
 getTemp();
